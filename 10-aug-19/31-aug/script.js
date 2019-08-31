32fab5511cf703fedf9b711842b12e6688dc2366
Nicolás M. Pardo <nicolasm@nikodermus.media>
2019-08-31 18:23:28 -0500

    clase de katas2

---
+++ 10-aug-19/31-aug/script.js
@@ -0,0 +1,39 @@
+/*const tabla = paramTabla => {
+  const number = Number.parseInt(paramTabla)
+  for (let cont = 0; cont <= 10; cont += 1) {
+    document.write(number + "x" + cont + "=" + cont * number + "</br>");
+  }
+}
+
+const pedirTabla = prompt("¿Cúal tabla quieres ver?", "4")
+
+tabla(pedirTabla);*/
+
+/*
+const sumador = listNumber => {
+  const array = listNumber.split(",");
+  for (let cont = 0; cont < array.length; cont += 1) {
+    array[cont] = Number.parseInt(array[cont])
+  }
+  let cuenta = 0;
+  for (let cont = 0; cont < array.length; cont += 1) {
+    cuenta = cuenta + array[cont];
+  }
+  document.write(cuenta)
+}
+const pedirNumeros = prompt("Cúales números quieres sumar?")
+sumador(pedirNumeros);*/
+
+// const listWord = userList => {
+//   for (let cont = 0; cont < userList.length; cont += 1) {
+//     console.log(userList[cont])
+//   }
+// }
+
+
+const listWord = userList => {
+  for (let cont = 0; cont < userList.length; cont += 1) {
+    if (userList[cont].length)
+
+  }
+}

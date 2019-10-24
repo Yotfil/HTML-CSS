aca590ff9a819caf8f0f2ea779abdf8f04c8b9cd
Nicolás M. Pardo <nicolasm@nikodermus.media>
2019-10-23 20:49:20 -0500

    Clase del 23 de oct

---
+++ array/script.js
@@ -0,0 +1,85 @@
+const people = [
+    {
+        name: 'Juan',
+        age: 19,
+        type: 'male'
+    },
+    {
+        name: 'Jose',
+        age: 19,
+        type: 'male'
+    },
+    {
+        age: 21,
+        name: 'Natalia',
+        type: 'female'
+    },
+    {
+        name: 'Tatiana',
+        age: 19,
+        type: 'female'
+    },
+    {
+        name: 'Camila',
+        type: 'female',
+        age: 19
+    },
+    {
+        age: 19,
+        name: 'Juana',
+        type: 'female'
+    }
+];
+
+const isOverage = (age) => {
+    return age >= 18;
+};
+
+const peopleCanDrink = () => {
+    let personCanDrink = [];
+
+    for (let i = 0; i < people.length; i += 1) {
+        // console.log(people[i]);
+        let person = people[i];
+        // para obtener una propiedad de un objeto se hace como està abajo
+        if (isOverage(person.age)) {
+            // console.log(person);
+            personCanDrink.push(person.name);
+        }
+    }
+    return personCanDrink;
+};
+
+const peopleInGroups = () => {
+    let personCantDrink = [];
+    let personCanDrink = [];
+    for (let i = 0; i < people.length; i += 1) {
+        let person = people[i];
+        if (isOverage(person.age)) {
+            personCanDrink.push(person.name);
+        } else {
+            personCantDrink.push(person.name);
+        }
+    }
+    return [personCantDrink, personCanDrink];
+};
+
+const allPeopleCanDrink = () => {
+    const personCantDrink = peopleInGroups()[0];
+    if (personCantDrink.length >= 1) {
+        return false;
+    } else {
+        return true;
+    }
+};
+
+const someoneCanDrink = () => {
+    debugger;
+    const personCanDrink = peopleInGroups()[1];
+    if (personCanDrink.length >= 1) {
+        return true;
+    } else {
+        return false;
+    }
+};
+// TAREA solo usando la funciòn peopleCanDrink hacer funcionar las ultimas dos funciones
